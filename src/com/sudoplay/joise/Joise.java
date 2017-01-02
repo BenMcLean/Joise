4f101360cfa64c41485a0e7cb767d08b346b8b7a
Benjamin McLean <mclean.ben@gmail.com>
2017-01-02 09:08:47 -0600

    Added reflect package from libGDX

--- src/com/sudoplay/joise/Joise.java
+++ src/com/sudoplay/joise/Joise.java
@@ -48,14 +48,15 @@
 
 package com.sudoplay.joise;
 
+import com.sudoplay.joise.module.Module;
+import com.sudoplay.joise.module.SeedableModule;
+import com.sudoplay.util.Assert;
+import com.sudoplay.util.reflect.ClassReflection;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map.Entry;
-
-import com.sudoplay.joise.module.Module;
-import com.sudoplay.joise.module.SeedableModule;
-import com.sudoplay.util.Assert;
 
 public class Joise {
 
@@ -108,7 +109,7 @@ public class Joise {
         Entry<String, ModulePropertyMap> e = it.next();
         ModulePropertyMap props = e.getValue();
         String moduleName = "com.sudoplay.joise.module." + props.get("module");
-        module = (Module) Class.forName(moduleName).newInstance();
+        module = (Module) ClassReflection.forName(moduleName).newInstance();
         module.buildFromPropertyMap(props, im);
         if (module instanceof SeedableModule
             && ((SeedableModule) module).hasSeedName()) {
